287446a74c7551eefd6ed1aee3f39fc91f42a256
Japneet Singh <singhjapneetssa@gmail.com>
2020-06-12 17:10:00 +0530

    changed title

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="./index.css" type="text/css">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>BSW IIT Delhi</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg">
