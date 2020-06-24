61db5d5aed7773c61b075f43173c9dc73e0355db
Japneet Singh <singhjapneetssa@gmail.com>
2020-06-25 00:27:53 +0530

    Notice v2

--- index.php
+++ index.php
@@ -199,6 +199,83 @@
 	.notice-ol{text-align:center;}
 	.notice-ol .notice-li{display:inline;padding-right:1em;}
 	.notice-ol .notice-li .notice-a{color:#fff;}
+	.quote-container {
+  margin-top: 50px;
+  position: relative;
+}
+
+.note {
+  color: #333;
+  position: relative;
+  width: 100%;
+  margin: 0 auto;
+  padding: 10px;
+  box-shadow: 0 10px 10px 2px rgba(0,0,0,0.1);
+  border-radius:10px;
+}
+
+.note .author {
+  display: block;
+  margin: 40px 0 0 0;
+  text-align: right;
+}
+
+.yellow {
+  background: #ffc;
+  -webkit-transform: rotate(2deg);
+  -moz-transform: rotate(2deg);
+  -o-transform: rotate(2deg);
+  -ms-transform: rotate(2deg);
+  transform: rotate(2deg);
+}
+.pin {
+  background-color: #aaa;
+  display: block;
+  height: 32px;
+  width: 2px;
+  position: absolute;
+  left: 50%;
+  top: -16px;
+  z-index: 1;
+}
+.pin:after {
+  background-color: #A31;
+  background-image: radial-gradient(25% 25%, circle, hsla(0,0%,100%,.3), hsla(0,0%,0%,.3));
+  border-radius: 50%;
+  box-shadow: inset 0 0 0 1px hsla(0,0%,0%,.1),
+              inset 3px 3px 3px hsla(0,0%,100%,.2),
+              inset -3px -3px 3px hsla(0,0%,0%,.2),
+              23px 20px 3px hsla(0,0%,0%,.15);
+  content: '';
+  height: 12px;
+  left: -5px;
+  position: absolute;
+  top: -10px;
+  width: 12px;
+}
+.pin:before {
+  background-color: hsla(0,0%,0%,0.1);
+  box-shadow: 0 0 .25em hsla(0,0%,0%,.1);
+  content: '';
+
+  height: 24px;
+  width: 2px;
+  left: 0;
+  position: absolute;
+  top: 8px;
+
+  transform: rotate(57.5deg);
+  -moz-transform: rotate(57.5deg);
+  -webkit-transform: rotate(57.5deg);
+  -o-transform: rotate(57.5deg);
+  -ms-transform: rotate(57.5deg);
+
+  transform-origin: 50% 100%;
+  -moz-transform-origin: 50% 100%;
+  -webkit-transform-origin: 50% 100%;
+  -ms-transform-origin: 50% 100%;
+  -o-transform-origin: 50% 100%;
+}
 
 
 
@@ -342,6 +419,48 @@
 
 			<div class="col-12 col-lg-8 notices  my-auto">
 					<div class="row justify-content-center">
+					
+					<div class="notice col-5 col-md-3 col-lg-3 my-2">
+						<div class="quote-container">
+						<i class="pin"></i>
+						<blockquote class="note yellow">
+						<h3>Blanket Camp</h3>
+						<h5>31 Oct 2019</h5>
+						<p>Sample text sample sample sample sample sample sample</p>
+						</blockquote>
+						</div>
+					</div>
+
+					<div class="notice   col-5 col-md-3 col-lg-3 my-2">
+						<div class="quote-container">
+						<i class="pin"></i>
+						<blockquote class="note yellow">
+						<h3>Blanket Camp</h3>
+						<h5>31 Oct 2019</h5>
+						<p>Sample text sample sample sample sample sample sample</p>
+						</blockquote>
+						</div>
+					</div>
+					<div class="notice    col-5 col-md-3 col-lg-3 my-2">
+						<div class="quote-container">
+						<i class="pin"></i>
+						<blockquote class="note yellow">
+						<h3>Blanket Camp</h3>
+						<h5>31 Oct 2019</h5>
+						<p>Sample text sample sample sample sample sample sample</p>
+						</blockquote>
+						</div>
+					</div>
+					<div class="notice   col-5 col-md-3 col-lg-3 my-2">
+						<div class="quote-container">
+						<i class="pin"></i>
+						<blockquote class="note yellow">
+						<h3>Blanket Camp</h3>
+						<h5>31 Oct 2019</h5>
+						<p>Sample text sample sample sample sample sample sample</p>
+						</blockquote>
+						</div>
+					</div>
 					<div class="notice   col-5 col-md-3 col-lg-3 my-2">
 						<div class="notice-inner">
 						<h3>Blanket Camp</h3>
@@ -370,33 +489,13 @@
 						<p>Sample text sample sample sample sample sample sample</p>
 						</div>
 					</div>
-					<div class="notice   col-5 col-md-3 col-lg-3 my-2">
-						<div class="notice-inner">
-						<h3>Blanket Camp</h3>
-						<h5>31 Oct 2019</h5>
-						<p>Sample text sample sample sample sample sample sample</p>
-						</div>
-					</div>
-					<div class="notice   col-5 col-md-3 col-lg-3 my-2">
-					<div class="notice-inner">
-						<h3>Blanket Camp</h3>
-						<h5>31 Oct 2019</h5>
-						<p>Sample text sample sample sample sample sample sample</p>
-						</div>
-					</div>
-					<div class="notice    col-5 col-md-3 col-lg-3 my-2">
-					<div class="notice-inner">
-						<h3>Blanket Camp</h3>
-						<h5>31 Oct 2019</h5>
-						<p>Sample text sample sample sample sample sample sample</p>
-						</div>
-					</div>
-					<div class="notice   col-5 col-md-3 col-lg-3 my-2">
-					<div class="notice-inner">
-						<h3>Blanket Camp</h3>
-						<h5>31 Oct 2019</h5>
-						<p>Sample text sample sample sample sample sample sample</p>
-						</div>
+
+					<div class="quote-container" style="width: 80%;">
+					<i class="pin"></i>
+					<blockquote class="note yellow">
+						We can't solve problems by using the same kind of thinking we used when we created them.
+						<cite class="author">Albert Einstein</cite>
+					</blockquote>
 					</div>
 					</div>
 					
