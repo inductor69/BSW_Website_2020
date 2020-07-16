a54292fd293676426047523eafaabc9a056e9c73
Japneet Singh <singhjapneetssa@gmail.com>
2020-07-16 19:12:48 +0530

    Added basic alumni_m page

---
+++ alumni_mentorship.php
@@ -0,0 +1,280 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 
+
+<title>Alumni Mentorship | BSW</title>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+</style>
+
+<style type="text/css">
+
+    html {
+        overflow-y: scroll
+    }
+    body{
+        background-color: rgba(68,215,182,0.18) !important;
+    }
+	
+	#page-header{
+        z-index: -1;
+        position: absolute;
+        margin-top:40px;
+	}
+
+    #am-heading{
+        margin-top:60px;
+        text-align: center !important;
+    }
+
+    @media (max-width: 769px){
+    #am-heading h1{
+        font-family: Montserrat;
+        font-weight: 500;
+        color: rgba(0,0,0,0.70);
+        text-shadow: 0 2px 4px rgba(0,0,0,0.50);
+        font-size:50px !important;
+    }
+    .quote{
+        font-weight: 300 !important;
+        font-size: 24px !important;
+    }
+    }
+    @media (min-width: 769px){
+    #am-heading h1{
+        font-family: Montserrat;
+        font-weight: 500;
+        color: rgba(0,0,0,0.70);
+        text-shadow: 0 2px 4px rgba(0,0,0,0.50);
+        font-size:80px !important;
+    }
+    .quote{
+        font-weight: 300 !important;
+        font-size: 30px !important;
+    }
+    }
+
+    #am-why{
+        color: rgba(0,0,0,0.70);
+    }
+    #am-how{
+        color: rgba(0,0,0,0.70);
+    }
+    #am-reg{
+        color: rgba(0,0,0,0.70);
+    }
+    #am-faq{
+        color: rgba(0,0,0,0.70);
+    }
+    .steps{
+        background: #FFFFFF;
+        box-shadow: 2px 2px 26px 10px rgba(0,0,0,0.08), inset 0 0 9px 8px rgba(152,152,152,0.05);
+        border-radius: 30px;
+        padding: 20px;
+        font-weight: 500;
+    }
+
+    @media (max-width: 769px){
+    .footer-img{
+        margin-top: -100px;
+    }
+    }
+    @media (min-width: 769px){
+    .footer-img{
+        margin-top: -200px;
+    }
+    }
+
+
+	
+</style>
+
+</head>
+
+
+<body >
+		
+<?php require 'components/header.php'; ?>
+<?php require 'components/back_to_top.php'; ?>
+
+
+<div id="page-header">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+
+            <div class="col-12">
+            <img src="images/alumni_m.svg" style="width: 100%;">
+            </div>
+
+		</div>	
+	</div>
+</div>
+
+<div class="container" id="am-heading">
+
+	<div class="container">
+	<div class="row">
+
+        <div class="col-9 offset-3 mt-4" style="text-align: center;">
+		<h1 class="my-4" style="font-family: Montserrat !important; ">Alumni Mentorship</h1>
+        </div>
+
+        <div class="col-12 col-md-9 offset-md-3 mb-4 quote" style="text-align: center;">
+        <p class="col-12" style="font-family: Montserrat !important; ">“The mind is not a vessel that needs filling, but wood that needs igniting.” </p>
+        <p class="col-12" style="font-family: Montserrat !important; color: #1155CC; font-weight: 500;">Mestrius Plutarchus</p>
+        </div>
+
+
+	</div>
+	</div>
+
+</div>
+
+<div class="container" id="am-why">
+
+	<div class="container">
+	<div class="row">
+
+        <div class="col-12" style="text-align: center;">
+		<h1 class="my-4" style="font-family: Montserrat !important; ">Why do we need this program?</h1>
+        <p style="font-size: 1.2rem">With the vast number of opportunities available in IITD, comes in a lot of confusion regarding what path to choose and in deciding what the ultimate goal is. In the process there is a huge chance to lose oneself and merge with the herd. That is when we realised that the people who themselves have faced this confusion first hand and have successfully gone through this phase will be the best people to guide and counsel the future generation. Under this programme, we invite our alumni from across the world and from different fields to interact with the students and help them build their network and gain a new perspective on the possibilities open to them after graduation. </p>
+        </div>
+
+	</div>
+	</div>
+
+</div>
+
+<div class="container" id="am-how">
+
+	<div class="container">
+
+	<div class="row">
+        <div class="col-12" style="text-align: center;">
+		<h1 class="my-4" style="font-family: Montserrat !important; ">How does this program work?</h1>
+        <p style="font-size: 1.2rem">The ‘Student Alumni Mentorship Program’ aims at establishing one on one connecting platform between students and alumni of IIT Delhi. This would be achieved by :</p>
+        </div>
+	</div>
+
+    <div class="row justify-content-center">
+
+        <div class="col-4 steps" style="text-align: center;">
+        <p>Present IITD students and IITD alumni need to fill our Exhaustive Registration Form</p>
+        </div>
+
+        <div class="col-7 steps ml-4" style="text-align: center;">
+        <p>Our profile matching software would scan through the responses received and match them based on the interested profile and future prospects stated by the student to the working profile of a mentor.</p>
+        </div>
+
+	</div>
+
+    <div class="row my-4 justify-content-center">
+
+        <div class="col-11 steps" style="text-align: center;">
+        <p>For those who couldn't be matched with a mentor , a repository containing the contact details of alumni of IITD across various fields would be made available who can be contacted by any student independently.</p>
+        </div>
+
+    </div>
+
+	</div>
+
+</div>
+
+<div class="container" id="am-why">
+
+	<div class="container">
+
+	<div class="row">
+        <div class="col-12" style="text-align: center;">
+		<h1 class="my-4" style="font-family: Montserrat !important; ">Why choose this program?</h1>
+        </div>
+	</div>
+
+    <div class="row justify-content-center mb-4">
+
+        <div class="col-12 col-md-5 steps my-2" style="text-align: center;">
+        <p><h1>For Mentors</h1>We believe that the versatile and dynamic alumni network of IIT Delhi is always willing to help motivated students in different career avenues. As an alumni mentor, you will have the opportunity to advise student(s) on academic guidance, career advice and personal development. You form meaningful, rewarding relationships that prepare students to be better employees and leaders, inspire them to stay connected with IITD, and motivate them to support the next generation of students. Mentors help perpetuate, grow, and strengthen IITD’s global alumni network. You do not have to be on campus to guide the mentees. </p>
+        </div>
+
+        <div class="col-12 col-md-5 steps ml-md-4 mt-4 mt-md-2" style="text-align: center;">
+        <p><h1>For Mentees</h1>As a mentee, you have exclusive access to a robust network of alumni who are eager and willing to meet you and share their wisdom and advice about life beyond IITD. The IITD Alumni Mentor Program is a unique opportunity for you to form meaningful and lasting relationships with alumni that will extend beyond campus walls. As a program participant, you’ll have a chance to request a mentor to assist you with academic guidance, career advice and personal development. It’s a valuable relationship that serves as an important complement to your classroom and action learning experiences, and makes for a richer IITD experience. </p>
+        </div>
+
+	</div>
+
+
+	</div>
+
+</div>
+
+<div class="container" id="am-reg">
+
+	<div class="container">
+
+	<div class="row">
+        <div class="col-12" style="text-align: center;">
+		<h1 class="my-4" style="font-family: Montserrat !important; ">How to register?</h1>
+        </div>
+	</div>
+
+    <div class="row justify-content-center mb-4">
+
+        <div class="col-12">
+        <p><h1>Mentor Registration</h1>1. Register/ Log in to https://iitd.almaconnect.com/mentorship  <br>
+        2. Fill out our form. Fill out all the details accurately as this response would be used to match you to your mentee <br>
+        3. Once we find you a match, we will let you know of your allotted mentees and the portal could also be used to establish meaningful connection.</p>
+        </div>
+
+        <div class="col-12" >
+        <p><h1>Mentee Registration</h1>1. Register/ Log in to https://iitd.almaconnect.com/mentorship <br>
+        2. Fill out our form. Fill out all the details accurately as this response would be used to match you to your mentor <br>
+        3. Once we find you a match, we will let you know of your allotted mentor and the portal could also be used to establish meaningful connection. <br>
+        </p>
+        </div>
+
+	</div>
+
+
+	</div>
+
+</div>
+
+<div class="container" id="am-faq">
+
+	<div class="container">
+
+	<div class="row">
+        <div class="col-12" style="text-align: center;">
+		<h1 class="my-4" style="font-family: Montserrat !important; ">Have some more questions? <br> Click here for FAQs.</h1>
+        </div>
+	</div>
+
+
+	</div>
+
+</div>
+
+<div class="col-12 footer-img">
+    <img src="images/footer_am.svg" style="width: 100%;">
+</div>
+
+
+<?php require 'components/footer.php'; ?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
